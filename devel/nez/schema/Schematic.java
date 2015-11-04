58c6618ec1b3f4ad5b278ab0a1edb4184cbbab36
yshinya6 <y.shinya.kml@gmail.com>
2015-11-04 14:38:18 +0900

    added schematic annotations

--- devel/nez/schema/Schematic.java
+++ devel/nez/schema/Schematic.java
@@ -13,3 +13,44 @@ import java.lang.annotation.Target;
 @Target(value = { TYPE, FIELD })
 public @interface Schematic {
 }
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = { TYPE })
+@interface Ordered {
+}
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = { TYPE })
+@interface Unordered {
+}
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = { FIELD })
+@interface Option {
+}
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = { FIELD })
+@interface Range {
+	int max = 0;
+	int min = 0;
+}
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = { FIELD })
+@interface Length {
+	int max = 0;
+	int min = 0;
+}
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = { FIELD })
+@interface Enumeration {
+	String[] value();
+}
