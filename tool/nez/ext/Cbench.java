f426e092f9b9c676a87d1eb1b8f6822d7f5010d1
yshinya6 <y.shinya.kml@gmail.com>
2015-11-17 23:36:44 +0900

    added Cbench

---
+++ tool/nez/ext/Cbench.java
@@ -0,0 +1,39 @@
+package nez.ext;
+
+import java.io.IOException;
+
+import nez.Parser;
+import nez.ast.CommonTree;
+import nez.ast.Tree;
+import nez.io.SourceContext;
+import nez.io.StringContext;
+import nez.main.Command;
+import nez.main.CommandContext;
+import nez.parser.ParsingMachine;
+import nez.util.ConsoleUtils;
+
+public class Cbench extends Command {
+
+	@Override
+	public void exec(CommandContext config) throws IOException {
+		Parser g = config.newParser();
+		int fileIndex = 0;
+
+		while (config.hasInput()) {
+			String file = config.getInputFileList().ArrayValues[fileIndex++];
+			Tree<?> prototype = config.getStrategy().isDisabled("ast", true) ? null : new CommonTree();
+			SourceContext input = config.nextInput();
+			for (int i = 0; i < 5; i++) {
+				g.perform(new ParsingMachine(), input, prototype);
+				// we assume there is no syntax error
+				if (input.hasUnconsumed()) {
+					ConsoleUtils.println(input.getUnconsumedMessage());
+					break;
+				}
+				input = StringContext.newFileContext(file);
+			}
+			g.logProfiler();
+		}
+	}
+
+}
